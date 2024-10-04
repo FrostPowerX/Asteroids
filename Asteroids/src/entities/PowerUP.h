4e3f913c1114f7fe9b94ee2f186aeb12eb58c32a
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-04 12:39:33 -0300

    Add PowerUp

---
+++ Asteroids/src/entities/PowerUP.h
@@ -0,0 +1,25 @@
+#ifndef POWER_UP
+#define POWER_UP
+
+#include "raylib.h"
+
+struct PowerUp
+{
+	Rectangle rect;
+
+	Color rectColor;
+
+	float addHeight;
+	float addSpeed;
+	int addBalls;
+
+	int toPlayer;
+
+	bool isActive;
+};
+
+PowerUp CreatePowerUp(Rectangle rect, Color rectColor, float heightPlus, float speedPlus, int ballsPlus);
+
+void DrawPowerUp(PowerUp& pUp);
+
+#endif // !POWER_UP

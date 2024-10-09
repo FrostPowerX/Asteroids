36117250514d0cbb7b4fbeb8eb83a1210df9d52b
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-09 10:44:57 -0300

    Modify SceneMenu

--- Asteroids/src/scenes/SceneMenu.cpp
+++ Asteroids/src/scenes/SceneMenu.cpp
@@ -11,27 +11,33 @@ namespace game
 	{
 		namespace menu
 		{
-			Button example;
+			const int maxButtons = 4;
+
+			Button buttons[maxButtons];
 
 			void Init()
 			{
-				example = CreateButton(0, 0, 50, 25, "Credits", 10, RED, WHITE, PURPLE, YELLOW);
+				//example = CreateButton(0, 0, 50, 25, "Credits", 10, RED, WHITE, PURPLE, YELLOW);
 			}
 
 			void Input()
 			{
+				MouseOnTopButton(buttons[0]);
+				if (IsButtonPressed(buttons[0]))
+					currentScene = SCENE::CREDITS;
 			}
 
 			void Update()
 			{
-				MouseOnTopButton(example);
-				if (IsButtonPressed(example))
-					currentScene = SCENE::CREDITS;
+				
 			}
 
 			void Draw()
 			{
-				DrawButton(example);
+				for (int i = 0; i < maxButtons; i++)
+				{
+					DrawButton(buttons[i]);
+				}
 			}
 
 			void DeInit()

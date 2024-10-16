7c282757c911361908138c3ef7e3e7dfb7035d43
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-16 10:39:58 -0300

    Add SceneControlls

---
+++ Asteroids/src/scenes/SceneControlls.cpp
@@ -0,0 +1,35 @@
+#include "SceneControlls.h"
+
+namespace game
+{
+	namespace scenes
+	{
+		namespace controlls
+		{
+
+
+			void Init()
+			{
+
+			}
+
+			void Input()
+			{
+
+			}
+			void Update()
+			{
+
+			}
+			void Draw() 
+			{
+
+			}
+
+			void DeInit()
+			{
+
+			}
+		}
+	}
+}

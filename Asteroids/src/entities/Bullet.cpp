53761e04700d264644dbf0d650d4f65f964e9eec
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-18 11:39:32 -0300

    Add Bullet.cpp Change Bullet.h

---
+++ Asteroids/src/entities/Bullet.cpp
@@ -0,0 +1,27 @@
+#include "Bullet.h"
+
+namespace bullet
+{
+	Bullet Create(Vector2 position, Vector2 direction, float speed, float dmg, bool isAlive)
+	{
+		return Bullet();
+	}
+
+	void Move(Bullet& bullet)
+	{
+	}
+
+	void Update(Bullet& bullet)
+	{
+		Move(bullet);
+	}
+	void Draw(Bullet bullet)
+	{
+	}
+
+	void OnCollision(Bullet& bullet)
+	{
+	}
+}
+
+
--- Asteroids/src/entities/Bullet.h
+++ Asteroids/src/entities/Bullet.h
@@ -6,7 +6,7 @@ namespace bullet
 {
 	struct Bullet
 	{
-		Vector2 direction;
+		Vector2 target;
 		Vector2 position;
 
 		float speed;
@@ -18,7 +18,9 @@ namespace bullet
 
 	Bullet Create(Vector2 position, Vector2 direction, float speed = 200, float dmg = 15, bool isAlive = true);
 
-	void Move(Bullet& bullet, Vector2 direction);
+	void Move(Bullet& bullet);
+
+	void Update(Bullet& bullet);
 	void Draw(Bullet bullet);
 
 	void OnCollision(Bullet& bullet);

8494d19edc9bd7149d2a179226d5d529fcb95303
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-18 12:56:47 -0300

    Change SceneGamePlay

--- Asteroids/src/scenes/SceneGamePlay.cpp
+++ Asteroids/src/scenes/SceneGamePlay.cpp
@@ -1,14 +1,23 @@
 #include "SceneGamePlay.h"
 
+#include "entities/SpaceShip.h"
+
+
 namespace game
 {
+	using namespace spaceship;
+
 	namespace scenes
 	{
 		namespace gameplay
 		{
-			void Init() 
-			{
+			SpaceShip player;
 
+			void CreatePlayer();
+
+			void Init()
+			{
+				CreatePlayer();
 			}
 
 			void Input()
@@ -17,16 +26,37 @@ namespace game
 			}
 			void Update()
 			{
-
+				Update(player);
 			}
 			void Draw()
 			{
-
+				Draw(player);
 			}
 
 			void DeInit()
 			{
 
+			}
+
+			void CreatePlayer()
+			{
+				float screenWidth = GetScreenWidth();
+				float screenHeight = GetScreenHeight();
+
+				Rectangle source{ 0,0,32,32 };
+				Rectangle dest{ 0,0,16,16 };
+				Vector2 origin{ 0,0 };
+
+				Circle cir;
+
+				std::string textureName = "PlayerShip";
+
+				Graphic graph{ Texture2D(),
+								source,
+								dest,
+								origin };
+
+				player = Create(cir, graph, textureName, 500, 800, 3, true);
 			}
 		}
 	}

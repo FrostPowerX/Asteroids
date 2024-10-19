64d02bd46400e159e7b94ac83c82187e30802dcb
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-19 17:35:34 -0300

    Change SpaceShip
    Add Move logic.
    Add Rotate Logic.
    Add InputLogic.

--- Asteroids/src/entities/SpaceShip.cpp
+++ Asteroids/src/entities/SpaceShip.cpp
@@ -1,5 +1,7 @@
 #include "SpaceShip.h"
 
+#include <math.h>
+
 #include "managers/InputManager.h"
 #include "managers/SpriteManager.h"
 #include "utilities/Utils.h"
@@ -12,10 +14,19 @@ namespace game
 		void Rotate(SpaceShip& sp, Vector2 target);
 		void Shoot(SpaceShip& sp);
 
+		void ActualizePos(SpaceShip& sp);
+		void NormalizeVelocity(SpaceShip& sp);
+
 		SpaceShip Create(Circle cir, Graphic graph, std::string textureName, float speed, float maxSpeed, int lives, bool isAlive)
 		{
 			SpaceShip ship;
 			ship.body = cir;
+
+			graph.dest.x = cir.x;
+			graph.dest.y = cir.y;
+
+			ship.velocity = Vector2{ 0,0 };
+			ship.rotationAngle = 0;
 
 			ship.graphic.source = graph.source;
 			ship.graphic.dest = graph.dest;
@@ -33,27 +44,58 @@ namespace game
 			return ship;
 		}
 
+		void Update(SpaceShip& sp)
+		{
+			Rotate(sp, GetMousePosition());
+
+			if (input::GetKey("Shoot"))
+				Shoot(sp);
+
+			if(input::GetKey("Move"))
+				Move(sp, GetMousePosition());
+
+			ActualizePos(sp);
+		}
+
+		void Draw(SpaceShip sp)
+		{
+			DrawCircle(sp.body.x, sp.body.y, sp.body.radius, RED);
+			DrawTexturePro(sp.graphic.spriteSheet, sp.graphic.source, sp.graphic.dest, sp.graphic.origin, sp.rotationAngle + 90, WHITE);
+		}
+
 		void Move(SpaceShip& sp, Vector2 target)
 		{
+			Vector2 normDir;
+			normDir.x = target.x - sp.body.x;
+			normDir.y = target.y - sp.body.y;
+			normDir = NormalizeVector(normDir);
 
+			sp.velocity.x += normDir.x * sp.speed * GetFrameTime();
+			sp.velocity.y += normDir.y * sp.speed * GetFrameTime();
+
+			float maxSpeedX = sp.maxSpeed;
+			float maxSpeedY = sp.maxSpeed;
+
+			maxSpeedX *= (sp.velocity.x > 0) ? 1 : -1;
+			maxSpeedY *= (sp.velocity.y > 0) ? 1 : -1;
+
+			sp.velocity.x = (abs(sp.velocity.x) > sp.maxSpeed) ? maxSpeedX : sp.velocity.x;
+			sp.velocity.y = (abs(sp.velocity.y) > sp.maxSpeed) ? maxSpeedY : sp.velocity.y;
 		}
 		void Rotate(SpaceShip& sp, Vector2 target)
 		{
-			float x = sp.body.x;
-			float y = target.y;
+			double x = target.x - sp.body.x;
+			double y = target.y - sp.body.y;
 
-			float r = sqrt(x * x + y * y);
+			double angle = atan(y / x);
 
-			float angle = atan(y / x);
-
-			x = r * cos(angle);
-			y = r * sin(angle);
+			angle = RadiansToGrades(angle);
 
 			if (x < 0 && y > 0)
 				angle += 180;
-			else if (x < 0 && y < 0)
+			else if (x < 0 && y <= 0)
 				angle += 180;
-			else if (x > 0 && y > 0)
+			else if (x > 0 && y < 0)
 				angle += 360;
 
 			sp.rotationAngle = angle;
@@ -63,17 +105,23 @@ namespace game
 			std::cout << "Disparo\n";
 		}
 
-		void Update(SpaceShip& sp)
+		void ActualizePos(SpaceShip& sp)
 		{
-			Rotate(sp, GetMousePosition());
-			Move(sp, GetMousePosition());
+			sp.body.x += sp.velocity.x * GetFrameTime();
+			sp.body.y += sp.velocity.y * GetFrameTime();
 
-			if (input::GetKey("Shoot"))
-				Shoot(sp);
+			sp.graphic.dest.x = sp.body.x;
+			sp.graphic.dest.y = sp.body.y;
 		}
-		void Draw(SpaceShip sp)
+		void NormalizeVelocity(SpaceShip& sp)
 		{
-			DrawTexturePro(sp.graphic.spriteSheet, sp.graphic.source, sp.graphic.dest, sp.graphic.origin, sp.rotationAngle, WHITE);
+			float magnitude = GetMagnitud(sp.velocity);
+			float normalVel = magnitude / sp.maxSpeed;
+
+			if (normalVel > 1.0)
+			{
+				sp.velocity = NormalizeVector(sp.velocity);
+			}
 		}
 	}
 

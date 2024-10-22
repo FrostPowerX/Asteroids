26d01503721c069be6e15af63f1bbf611648e6e9
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-21 23:43:13 -0300

    Add Graphic Struct and default texture Size on SpriteManager

--- Asteroids/src/managers/SpriteManager.h
+++ Asteroids/src/managers/SpriteManager.h
@@ -9,12 +9,23 @@ namespace game
 {
 	namespace spritemanager
 	{
+		const int textureWidth = 32;
+		const int textureHeight = 32;
+
 		struct Sprite
 		{
 			Texture2D texture;
 			std::string name;
 		};
 
+		struct Graphic
+		{
+			Texture2D spriteSheet;
+
+			Rectangle source;
+			Rectangle dest;
+			Vector2 origin;
+		};
 
 		Sprite GetSprite(std::string name);
 

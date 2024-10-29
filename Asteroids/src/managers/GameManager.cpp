5221228bbb1b8ed9b32fc76780c0e0b89f8cab60
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-28 21:54:07 -0300

    Add GameManager

---
+++ Asteroids/src/managers/GameManager.cpp
@@ -0,0 +1,32 @@
+#include "GameManager.h"
+
+namespace game
+{
+	namespace gamemanager
+	{
+		int actualScore = 0;
+		int maxScore = 0;
+
+		void ResetScore()
+		{
+			if (actualScore > maxScore)
+				maxScore = actualScore;
+			else
+				actualScore = 0;
+		}
+
+		void AddScore(int score)
+		{
+			actualScore += score;
+		}
+
+		int GetScore()
+		{
+			return actualScore;
+		}
+		int GetMaxScore()
+		{
+			return maxScore;
+		}
+	}
+}
---
+++ Asteroids/src/managers/GameManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace game
+{
+	namespace gamemanager
+	{
+		void ResetScore();
+
+		void AddScore(int score);
+
+		int GetScore();
+		int GetMaxScore();
+	}
+}

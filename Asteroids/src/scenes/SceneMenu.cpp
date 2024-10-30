2714ca77efe1a3bb40f718d5cfb063ca059ed63c
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-29 23:29:43 -0300

    Update SceneMenu

--- Asteroids/src/scenes/SceneMenu.cpp
+++ Asteroids/src/scenes/SceneMenu.cpp
@@ -5,6 +5,8 @@
 #include "GameLoop.h"
 
 #include "managers/ResolutionManager.h"
+#include "managers/SoundManager.h"
+#include "managers/GameManager.h"
 
 #include "ui/Button.h"
 
@@ -23,21 +25,27 @@ namespace game
 			Button buttons[maxButtonsPMenu];
 			std::string bNames[maxButtonsPMenu];
 
+			text::Text hightScore;
+
 			void Init()
 			{
+				soundmanager::PlayM("MenuM");
+
+				hightScore = text::Create("High Score        ", (GetScreenWidth() / 2), 30, 20, WHITE);
+
 				float offset = (ButtonHeight * 2.f) * GetScale().y;
 				float posx = currentWidth / 2.f;
 				float posy = offset;
 
 				bNames[0] = "Play";
-				bNames[1] = "Controlls";
+				bNames[1] = "Options";
 				bNames[2] = "Credits";
 				bNames[3] = "Exit";
 
 				for (int i = 0; i < maxButtonsPMenu; i++)
 				{
 					buttons[i] = Create("Button", Rectangle{ posx, posy, ButtonWidth, ButtonHeight }, bNames[i]);
-					
+
 					posy += offset;
 				}
 			}
@@ -49,12 +57,17 @@ namespace game
 					MouseOnTop(buttons[i]);
 					if (IsPressed(buttons[i]))
 						currentScene = SCENE(i);
+					if (currentScene == SCENE::GAMEPLAY)
+					{
+						soundmanager::StopM("MenuM");
+						soundmanager::PlayM("GamePlayM");
+					}
 				}
 			}
 
 			void Update()
 			{
-				
+				hightScore.text = "High Score: " + std::to_string(gamemanager::GetMaxScore());
 			}
 
 			void Draw()
@@ -63,6 +76,9 @@ namespace game
 				{
 					Draw(buttons[i]);
 				}
+
+				if (gamemanager::GetMaxScore() > 0)
+					text::Draw(hightScore);
 			}
 
 			void DeInit()

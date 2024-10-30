19ae9558fc823d87b1bf7680e37b82917054c98e
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-10-29 23:25:18 -0300

    Add Sounds and Music

---
+++ Asteroids/src/managers/SoundManager.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+
+#include "raylib.h"
+
+namespace game
+{
+	namespace soundmanager
+	{
+		struct SoundBox
+		{
+			Sound music;
+			std::string name;
+
+			bool loop;
+		};
+
+		struct MusicBox
+		{
+			Music music;
+			std::string name;
+
+			bool loop;
+		};
+
+		extern float volume;
+
+		void Update();
+
+		void PlayS(std::string name);
+		void StopS(std::string name);
+
+		void PlayM(std::string name);
+		void StopM(std::string name);
+
+		void LoadSounds();
+		void UnloadSounds();
+	}
+}
